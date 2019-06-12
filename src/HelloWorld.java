f22c7f98655637ac9524d87edb7a0fc549e013e3
greendb16 <greendb16@gmail.com>
2019-06-12 10:59:11 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+
+public class HelloWorld {
+    public static void  main(String[]args){
+        System.out.println("Hello, World!");
+        System.out.println("Today is July 19, 2015. I Am alive!");
+        System.out.println("My name is Dan");
+    }
+}
